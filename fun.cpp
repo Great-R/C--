1ceb83f11c7ba1e8ddf3ecd6974e3b77314f72a7
Great-R <2240111704@qq.com>
2025-05-09 13:01:19 +0800

    ctest

---
+++ fun.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+void output(){
+    int i = 0;
+    while(i<10){
+        std::cout<<i;
+        i++;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"hh.h"
+// Removed invalid namespace declaration
+int main(){
+    int i =0;
+    std::cout<<"hello world";
+    output();
+    return 0;
+}
